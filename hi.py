e8366323b525ff93baef408419601ad32720ce8c
Zeba Siddique <zebasiddiquemdgr@gmail.com>
2026-07-02 20:42:02 +0530

    Palindrome Number

--- hi.py
+++ hi.py
@@ -9,9 +9,29 @@ count = 0
 for i in range(1, n + 1):
     if n%i == 0:
         count += 1
-        
+
 if count == 2:
     print(n, "is a prime number")
 else:
     print(n, "is not a prime number")   
  
+
+
+
+
+
+#Palindrome number
+
+num = int(input("Enter a number: "))
+temp = num
+rev = 0
+
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num = num // 10
+
+if temp == rev:
+    print("Palindrome Number")
+else:
+    print("Not a Palindrome Number")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,16 @@
 
-#prime number
+#Palindrome number
 
-n = int(input("Enter a number: "))
-count = 0
-for i in range(1, n + 1):
-    if n%i == 0:
-        count += 1
-        
-if count == 2:
-    print(n, "is a prime number")
+num = int(input("Enter a number: "))
+temp = num
+rev = 0
+
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num = num // 10
+
+if temp == rev:
+    print("Palindrome Number")
 else:
-    print(n, "is not a prime number")   
- 
+    print("Not a Palindrome Number")
